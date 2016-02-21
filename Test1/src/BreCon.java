47ab56a32d0bfbc97ea49013427e966a5f2b10da
benjamin <benjamin@enjamin.Speedport_W_724V_Typ_A_05011603_00_007>
2016-02-21 12:33:44 +0100

    modified Class BreCon

--- Test1/src/BreCon.java
+++ Test1/src/BreCon.java
@@ -1,4 +1,15 @@
 
 public class BreCon {
 
+	public static int fakul(int zahl) {
+		int result = 1;
+		while (true) {
+			if (zahl < 1) {
+				break;
+			}
+			result *= zahl;
+			zahl--;
+		}
+		return result;
+	}
 }
