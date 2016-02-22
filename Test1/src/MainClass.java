0dd3fd09303b225a546b0bde28129c8f8a3f1e2d
benjamin <benjamin@enjamin.Speedport_W_724V_Typ_A_05011603_00_007>
2016-02-22 18:27:32 +0100

    modified Class MainClass

--- Test1/src/MainClass.java
+++ Test1/src/MainClass.java
@@ -1,12 +1,16 @@
 
 public class MainClass {
-	 static BreCon fakultet = new BreCon();
-
+	static BreCon fakultet = new BreCon();
+	static TypTyp vergleich = new TypTyp();
 
 	public static void main(String[] args) {
-		int erg;
-		erg = fakultet.fakul(4);
-		System.out.println(erg);
+		int erg1;
+		erg1 = fakultet.fakul(4);
+		System.out.println(erg1);
+
+		int erg2;
+		erg2 = vergleich.bool(10, 5);
+		System.out.println(erg2);
 
 	}
 
