3b9e2e8858ac62644a595ad245ed51f4484c6f50
benjamin <benjamin@enjamin.Speedport_W_724V_Typ_A_05011603_00_007>
2016-02-22 20:08:19 +0100

    modified Class SwitchFall

--- Test1/src/SwitchFall.java
+++ Test1/src/SwitchFall.java
@@ -1,4 +1,21 @@
 
 public class SwitchFall {
 
+	public void faelle(String name) {
+		switch (name) {
+		case "Hund":
+			System.out.println("Murat ist gemeint.");
+			break;
+		case "Lauch":
+			System.out.println("Nahro ist gemeint.");
+			break;
+		case "Funktion0":
+			System.out.println("Khaled ist gemeint.");
+			break;
+		default:
+			System.out.println("Falsche Name.");
+			break;
+		}
+	}
+
 }
