cd4326e95af996e4ecb57b1d0fd02b67ec48a3b9
benjamin <benjamin@enjamin.Speedport_W_724V_Typ_A_05011603_00_007>
2016-02-23 08:52:45 +0100

    modified Class ZufallZahl

--- Test1/src/ZufallZahl.java
+++ Test1/src/ZufallZahl.java
@@ -1,4 +1,16 @@
+import java.util.Random;
 
 public class ZufallZahl {
+
+	public int zufall() {
+		Random wurfel = new Random();
+
+		int zahl = 0;
+
+		zahl = wurfel.nextInt(5) + 1;
+
+		return zahl;
+
+	}
 
 }
