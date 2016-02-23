1f5fe44cdadeeaf1f1e859e8adec0671a7feea5d
benjamin <benjamin@enjamin.Speedport_W_724V_Typ_A_05011603_00_007>
2016-02-23 10:01:23 +0100

    modified Calss MainClass

--- Test1/src/MainClass.java
+++ Test1/src/MainClass.java
@@ -8,40 +8,53 @@ public class MainClass {
 		/*
 		 * 
 		 */
-		/*
-		 * BreCon fakultet = new BreCon(); int erg1; erg1 = fakultet.fakul(4);
-		 * System.out.println(erg1);
-		 */
+
+		// BreCon fakultet = new BreCon();
+		// int erg1;
+		// erg1 = fakultet.fakul(4);
+		// System.out.println(erg1);
 
 		/*
 		 * 
 		 */
-		/*
-		 * TypTyp vergleich = new TypTyp();
-		 * System.out.println(vergleich.bool(11, 13));
-		 */
+		// TypTyp vergleich = new TypTyp();
+		// System.out.println(vergleich.bool(11, 13));
 
 		/*
 		 * 
 		 */
-		/*
-		 * int[] arry = new int[5]; System.out.println(arry.length);
-		 */
+		// int[] arry = { 4, 5, 6, 9, 10 };
+		// System.out.println(arry.length);
+		// for (int item : arry) {
+		// item++;
+		// System.out.println(item);
+		// }
 
 		/*
 		 * 
 		 */
-		/*
-		 * SwitchFall typsuchen = new SwitchFall(); String name;
-		 * System.out.println("Geben Sie einen Typ ein:"); name = scan.next();
-		 * System.out.println(typsuchen.faelle(name));
-		 */
+		// int zahl1, zahl2;
+		// System.out.println("Geben Sie eine Zahl ein:");
+		// zahl1 = scan.nextInt();
+		// System.out.println("Geben Sie eine Zahl ein:");
+		// zahl2 =scan.nextInt();
+		// String text = zahl1 < zahl2 ? "true" : "false";
+		// System.out.println(text);
 
 		/*
 		 * 
 		 */
-		ZufallZahl wurfel = new ZufallZahl();
-		System.out.println(wurfel.zufall());
+		// SwitchFall typsuchen = new SwitchFall();
+		// String name;
+		// System.out.println("Geben Sie einen Typ ein:");
+		// name = scan.next();
+		// System.out.println(typsuchen.faelle(name));
+
+		/*
+		 * 
+		 */
+		// ZufallZahl wurfel = new ZufallZahl();
+		// System.out.println(wurfel.zufall());
 
 	}
 
